4ea7cadcb2f2d26d2d48d98bcbbf6152e2581e55
Waleed Esmail <waleed.physics@gmail.com>
2025-01-09 11:38:36 +0100

    Use NumPy memory mapping instead of H5 files

--- training/data/data_handling.py
+++ training/data/data_handling.py
@@ -25,7 +25,7 @@ class H5Dataset(Dataset):
         self.file = h5py.File(data_dir, "r")
 
         self.features = self.file["feature_matrices"]
-        self.sparse_adj_matrix = self.file["adj_matrices"]
+        self.adj_matrices = self.file["adj_matrices"]
         self.mask = self.file["mask"]
         self.labels = self.file["labels"]
 
@@ -34,11 +34,11 @@ class H5Dataset(Dataset):
         self.max_num_particles = node_features.shape[0]
 
         # Reconstruct the full adj matrix
-        self.reconstruct_adjacency(self.sparse_adj_matrix[item])
+        # self.reconstruct_adjacency(self.adj_matrices[item])
 
         return {
             "node_features": node_features,
-            "edge_features": torch.tensor(self.adj_matrix, dtype=torch.float32),
+            # "edge_features": torch.tensor(self.adj_matrix, dtype=torch.float32),
             "mask": torch.tensor(self.mask[item], dtype=torch.float32),
             "labels": torch.tensor(self.labels[item], dtype=torch.float32),
         }
@@ -63,7 +63,7 @@ class H5Dataset(Dataset):
         )
 
         # Extract the non-padded values for the current feature
-        valid_values = flat_adj_matrix[np.where(flat_adj_matrix[:, 0] != -1.0)[0], :]
+        valid_values = flat_adj_matrix[np.where(flat_adj_matrix[:, 0] > -999.0)[0], :]
         num_particles = self.infer_num_particles_from_pairs(valid_values.shape[0])
 
         # Get the upper triangle indices
@@ -84,6 +84,100 @@ class H5Dataset(Dataset):
         self.file.close()
 
 
+class MemMapDataset(Dataset):
+    def __init__(self, data_dir: str, max_num_particles: int):
+        """
+        Args:
+            data_dir (str): Path to the H5 file.
+        """
+        self.data_dir = data_dir
+        self.max_num_particles = max_num_particles
+        self.label_memmap = np.memmap(data_dir + "_labels.npy", dtype="int16", mode="r")
+        self.size = self.label_memmap.shape[0]
+
+        self.feature_matrix_memmap = np.memmap(
+            data_dir + "_feature_matrix.npy",
+            dtype="float32",
+            mode="r",
+            shape=(self.size, max_num_particles, 11),
+        )
+        self._adj_matrix_memmap = np.memmap(
+            data_dir + "_adj_matrix.npy",
+            dtype="float32",
+            mode="r",
+            shape=(self.size, max_num_particles * (max_num_particles - 1) // 2, 4),
+        )
+        self.mask_memmap = np.memmap(
+            data_dir + "_mask.npy",
+            dtype="int16",
+            mode="r",
+            shape=(self.size, max_num_particles),
+        )
+
+    def __len__(self) -> int:
+        """
+        Total number of samples in the dataset.
+        """
+        return self.size
+
+    def __getitem__(self, item) -> dict:
+        self.reconstruct_adjacency(self._adj_matrix_memmap[item])
+        return {
+            "node_features": torch.tensor(
+                self.feature_matrix_memmap[item], dtype=torch.float32
+            ),
+            "edge_features": torch.tensor(self.adj_matrix, dtype=torch.float32),
+            "mask": torch.tensor(self.mask_memmap[item], dtype=torch.float32),
+            "labels": torch.tensor(self.label_memmap[item], dtype=torch.float32),
+        }
+        return {"node_features": torch.tensor(self.feature_matrix_memmap[item])}
+
+    @staticmethod
+    def infer_num_particles_from_pairs(pairs: int) -> int:
+        """
+        Infer the number of particles from the number of pairs.
+
+        Args:
+            pairs (int): Number of pairs in the adjacency matrix.
+
+        Returns:
+            int: Number of particles.
+        """
+        # Solve n * (n - 1) / 2 = pairs
+        discriminant = 1 + 8 * pairs
+        n = int((-1 + math.sqrt(discriminant)) / 2)
+        return n + 1
+
+    def reconstruct_adjacency(self, flat_adj_matrix):
+        """
+        Reconstruct the adjacency matrix from its flattened form.
+
+        Args:
+            flat_adj_matrix (np.ndarray): Flattened adjacency matrix.
+            max_num_particles (int): Maximum number of particles in the graph.
+
+        Returns:
+            np.ndarray: Reconstructed adjacency matrix.
+        """
+        self.adj_matrix = np.zeros(
+            (self.max_num_particles, self.max_num_particles, flat_adj_matrix.shape[1])
+        )
+
+        # Extract the non-padded values for the current feature
+        valid_values = flat_adj_matrix[np.where(flat_adj_matrix[:, 0] > -999.0)[0], :]
+        num_particles = self.infer_num_particles_from_pairs(valid_values.shape[0])
+
+        # Get the upper triangle indices
+        triu_indices = np.triu_indices(num_particles, k=1)
+        for feature in range(valid_values.shape[1]):
+            self.adj_matrix[triu_indices[0], triu_indices[1], feature] = valid_values[
+                :, feature
+            ]
+
+        # Symmetrize the matrix
+        self.adj_matrix[:, :, :] += self.adj_matrix[:, :, :].transpose(1, 0, 2)
+
+
 class JetTaggingDataModule(LightningDataModule):
     """
     PyTorch Lightning DataModule for managing training, validation, and test datasets
@@ -99,6 +193,7 @@ class JetTaggingDataModule(LightningDataModule):
         self,
         data_dir: str,
         file_list: list,
+        max_num_particles: int = 100,
         batch_size: int = 32,
         num_workers: int = 8,
     ) -> None:
@@ -110,6 +205,7 @@ class JetTaggingDataModule(LightningDataModule):
             )
 
         self.train_file, self.val_file, self.test_file = file_list
+        self.max_num_particles = max_num_particles
         self.data_dir = data_dir
         self.batch_size = batch_size
         self.num_workers = num_workers
@@ -124,9 +220,18 @@ class JetTaggingDataModule(LightningDataModule):
         Args:
             stage (str): One of 'fit', 'validate', 'test', or 'predict'.
         """
-        self.train_dataset = H5Dataset(data_dir=self.data_dir + self.train_file)
-        self.val_dataset = H5Dataset(data_dir=self.data_dir + self.val_file)
-        self.test_dataset = H5Dataset(data_dir=self.data_dir + self.test_file)
+        self.train_dataset = MemMapDataset(
+            data_dir=self.data_dir + self.train_file,
+            max_num_particles=self.max_num_particles,
+        )
+        self.val_dataset = MemMapDataset(
+            data_dir=self.data_dir + self.val_file,
+            max_num_particles=self.max_num_particles,
+        )
+        self.test_dataset = MemMapDataset(
+            data_dir=self.data_dir + self.test_file,
+            max_num_particles=self.max_num_particles,
+        )
 
     def train_dataloader(self) -> DataLoader:
         """
@@ -135,6 +240,7 @@ class JetTaggingDataModule(LightningDataModule):
         Returns:
             DataLoader: PyTorch DataLoader for training.
         """
+
         return DataLoader(
             self.train_dataset,
             batch_size=self.batch_size,
@@ -150,6 +256,7 @@ class JetTaggingDataModule(LightningDataModule):
         Returns:
             DataLoader: PyTorch DataLoader for validation.
         """
+
         return DataLoader(
             self.val_dataset,
             batch_size=self.batch_size,
--- training/models/models.py
+++ training/models/models.py
@@ -11,8 +11,8 @@ from lightning.pytorch.utilities.types import STEP_OUTPUT
 from torchmetrics import Accuracy
 
 # Framework imports
-from models.transformer import mlp, Embedding, TransformerEncoder
-from models.gnn import EEDGCNEncoder
+from models.transformer import mlp, Embedding, TransformerEncoder, TransformerLayer
+from models.gnn import EEDGCNEncoder, EdgeConvWithEdgeFeatures
 
 
 class IAFormer(nn.Module):
@@ -125,6 +125,190 @@ class IAFormer(nn.Module):
         return output
 
 
+class IAFormer_old(nn.Module):
+    def __init__(
+        self,
+        f_dim,
+        n_particles,
+        U_features,
+        k=7,
+        n_layers=2,
+        h_dim=200,
+        expansion_factor=4,
+        n_heads=10,
+        masked=True,
+        pooling="avg",
+        embed_dim=[128, 512, 128],
+        U_dim=[128, 64, 64, 10],
+        mlp_f_dim=[128, 64],
+    ):
+        super(IAFormer_old, self).__init__()
+
+        """
+        Args:
+           f_dim: int, number  of the feature tokens
+           n_particles: int, number  of the particle tokens
+           U_features: int, number of the featires in the pairwise interaction matrix
+           n_Transformer: int, number of Transformer layers
+           h_dim: int, hidden dim of the Q,K and V
+           expansion_factor: int, expansion of the size of the internal MLP layers in the Transformer layers.
+           n_heads: int, number of attention heads
+           masked: boolean, to use the attention mask
+           Pooling: str, define the pooling kind, avg, max and sum
+           embed_dim: list, define the number of neurons in the MLP for features embedding
+           U_dim: list, define the number of neuron in the MLP for pairwise interaction embedding.
+                                                                      The last number must equals the number of attention heads
+           mlp_f_dim: list, define the number of neurons in the final MLP   
+         
+        return:
+                transformer netwirk with pairwise interaction matrix included.
+        """
+        self.f_dim = f_dim
+        self.n_particles = n_particles
+        self.U_features = U_features
+        self.k = k
+        self.n_layers = n_layers
+        self.n_heads = n_heads
+        self.masked = masked
+        self.expansion_factor = expansion_factor
+        self.h_dim = h_dim
+        self.pooling = pooling
+        self.embed_dim = embed_dim
+        self.mlp_f_dim = mlp_f_dim
+        self.U_dim = U_dim
+        self.mlp = mlp(self.n_particles, self.mlp_f_dim)
+        self.U_embeding = Embedding(self.U_features, self.U_dim)
+        self.embed = Embedding(self.f_dim, self.embed_dim)
+        self.layers_T = nn.ModuleList(
+            [
+                TransformerLayer(
+                    self.embed_dim[-1],
+                    self.h_dim,
+                    self.expansion_factor,
+                    self.n_heads,
+                    self.masked,
+                )
+                for _ in range(self.n_layers)
+            ]
+        )
+        self.layers_G = nn.ModuleList(
+            [
+                EdgeConvWithEdgeFeatures(
+                    self.embed_dim[-1],
+                    self.embed_dim[-1],
+                    self.U_dim[-1],
+                    self.k,
+                    self.pooling,
+                )
+                for _ in range(self.n_layers)
+            ]
+        )
+        self.nW = nn.Linear(self.U_dim[-1], self.n_heads)
+        self.nH = nn.LayerNorm(self.f_dim)
+        self.nE = nn.LayerNorm(self.U_features)
+
+    def forward(self, input_T, input_E):
+        """
+        input_T: dim (batch, particle tokens, feature tokens)
+        input_E: dim (batch, particle tokens, particle tokens, pairwise features)
+        """
+        input_E_ = self.nE(input_E)
+        input_T_ = self.nH(input_T)
+        inp_E = self.U_embeding(input_E_)
+        inp_T = self.embed(input_T_)
+        inp_H = self.embed(input_T_)
+
+        for gnn_layer, attention_layer in zip(self.layers_G, self.layers_T):
+
+            inp_H, inp_E = gnn_layer(inp_H, inp_E)
+
+            inp_E_T = torch.permute(self.nW(inp_E), (0, -1, 1, 2))
+            inp_T = attention_layer(inp_T, inp_E_T)
+
+        if self.pooling == "avg":
+            Transformer_output = inp_T.mean(dim=2)
+            out_H_ = inp_H.mean(dim=2)
+
+        elif self.pooling == "max":
+            Transformer_output = inp_T.max(dim=2)[0]
+            out_H_ = inp_H.max(dim=2)[0]
+
+        elif self.pooling == "sum":
+            Transformer_output = inp_T.sum(dim=2)
+            out_H_ = inp_H.sum(dim=2)
+
+        output_c = (
+            Transformer_output + out_H_
+        )  # torch.cat((Transformer_output,out_H_),dim=-1)
+        output = self.mlp(output_c)
+
+        return output
+
+
+class ParticleAttentionBlock(nn.Module):
+    def __init__(self, embed_dim, expansion_factor=4, num_heads=1):
+        super(ParticleAttentionBlock, self).__init__()
+        self.norm1 = nn.LayerNorm(embed_dim)
+        self.norm2 = nn.LayerNorm(embed_dim)
+        self.pmha = nn.MultiheadAttention(
+            embed_dim=embed_dim, num_heads=num_heads, batch_first=True
+        )
+        self.mlp = nn.Sequential(
+            nn.LayerNorm(embed_dim),
+            nn.Linear(embed_dim, expansion_factor * embed_dim),
+            nn.GELU(),
+            nn.LayerNorm(expansion_factor * embed_dim),
+            nn.Linear(expansion_factor * embed_dim, embed_dim),
+        )
+
+    def forward(self, x, u=None):
+        x_res = x
+        x = self.norm1(x)
+        attn_output, _ = self.pmha(
+            x, x, x, need_weights=False, attn_mask=u.flatten(start_dim=0, end_dim=1)
+        )
+        x = self.norm2(attn_output)
+        h = x + x_res
+        z = self.mlp(h)
+        return z + h
+
+
+class ParticleTransformer(nn.Module):
+    def __init__(
+        self,
+        feat_particles,
+        feat_interaction,
+        embed_dim,
+        num_heads,
+        num_blocks,
+        num_classes=1,
+    ):
+        super(ParticleTransformer, self).__init__()
+        assert embed_dim % num_heads == 0, "embed_dim must be divisible by num_heads"
+        self.particle_embed = nn.Linear(feat_particles, embed_dim)
+        self.interaction_embed = nn.Linear(feat_interaction, num_heads)
+        self.blocks = nn.ModuleList(
+            [
+                ParticleAttentionBlock(embed_dim=embed_dim, num_heads=num_heads)
+                for _ in range(num_blocks)
+            ]
+        )
+        self.mlp_head = nn.Sequential(nn.Linear(embed_dim, num_classes))
+
+    def forward(self, particles, interactions):
+        x = self.particle_embed(particles)
+        u = self.interaction_embed(interactions).transpose(3, 1)
+
+        for block in self.blocks:
+            x = block(x, u)
+
+        # Aggregate features (e.g., mean pooling)
+        x = x.mean(dim=1)  # Pool across particles
+
+        logits = self.mlp_head(x)
+        return logits
+
+
 class JetTaggingModule(LightningModule):
     """
     PyTorch Lightning Module for training a jet tagging model using EdgeConv.
@@ -179,21 +363,28 @@ class JetTaggingModule(LightningModule):
         # self.save_hyperparameters()
 
         # Create an instance of the model
-        self.model = IAFormer(
-            self.D,
-            self.N,
-            self.f,
-            self.n_Transformer,
-            self.n_GNN,
-            self.k,
-            expansion_factor=self.expansion_factor,
-            n_heads=self.n_heads,
-            masked=self.masked,
-            pooling=self.pooling,
-            embed_dim=self.embed_dim,
-            h_dim=self.h_dim,
-            U_dim=self.U_dim,
-            mlp_f_dim=self.mlp_f_dim,
+        # self.model = IAFormer_old(
+        #    self.D,
+        #    self.N,
+        #    self.f,
+        #    self.k,
+        #    n_layers=self.n_Transformer,
+        #    # self.n_GNN,
+        #    expansion_factor=self.expansion_factor,
+        #    n_heads=self.n_heads,
+        #    masked=self.masked,
+        #    pooling=self.pooling,
+        #    embed_dim=self.embed_dim,
+        #    h_dim=self.h_dim,
+        #    U_dim=self.U_dim,
+        #    mlp_f_dim=self.mlp_f_dim,
+        # )
+        self.model = ParticleTransformer(
+            feat_particles=11,
+            feat_interaction=4,
+            embed_dim=128,
+            num_heads=8,
+            num_blocks=8,
         )
 
         # Accuarcy matric
@@ -218,7 +409,7 @@ class JetTaggingModule(LightningModule):
         )
         model_out = self.model(node_features, edge_features).flatten()
         # calculate the loss
-        loss = torch.nn.functional.binary_cross_entropy(model_out, labels)
+        loss = torch.nn.functional.binary_cross_entropy_with_logits(model_out, labels)
         acc = self.accuracy(model_out, labels)
         self.log("loss", loss, prog_bar=True, sync_dist=True)
         self.log("acc", acc, prog_bar=True, sync_dist=True)
@@ -243,7 +434,9 @@ class JetTaggingModule(LightningModule):
         )
         model_out = self.model(node_features, edge_features).flatten()
         # calculate the loss
-        val_loss = torch.nn.functional.binary_cross_entropy(model_out, labels)
+        val_loss = torch.nn.functional.binary_cross_entropy_with_logits(
+            model_out, labels
+        )
         val_acc = self.accuracy(model_out, labels)
         self.log("val_loss", val_loss, prog_bar=True, sync_dist=True)
         self.log("val_acc", val_acc, prog_bar=True, sync_dist=True)
